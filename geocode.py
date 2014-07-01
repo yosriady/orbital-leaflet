4078ae78e21ea9746f27b2ee1dd312ee13cda20a
Yos Riady <yosriady@gmail.com>
2014-07-01 16:20:54 +0800

    Example Geocoder script using the Mapbox Geocoding API

--- geocode.py
+++ geocode.py
@@ -1,8 +1,8 @@
+import sys
 import requests
 
 GEOCODE_API_URL = 'http://api.tiles.mapbox.com/v3/%s/geocode/%s.json'
 GEOCODE_API_KEY = 'examples.map-zr0njcqy'
-
 
 def geocode(query):
     url = GEOCODE_API_URL % (GEOCODE_API_KEY, query)
@@ -11,4 +11,5 @@ def geocode(query):
     return dict
 
 if __name__ == "__main__":
-    print geocode("Jakarta")
+    for arg in sys.argv:
+        print geocode(arg)
